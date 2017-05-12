461e0760d059e2638937af205348d50f61c5a018
sezn <sezinengur@gmail.com>
2017-05-13 01:14:53 +0300

    TodoList first comment.

---
+++ todolist/todos/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Todo
+
+admin.site.register(Todo) # django admin e Todoyu ekledk.yaratgmz gbi title ve text i vars
